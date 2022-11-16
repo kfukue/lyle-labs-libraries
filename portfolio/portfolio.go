1fcd59466b7de44f74df9461270bdbd94ab9c007
Keita Fukue <keitafukue@gmail.com>
2022-11-16 11:32:03 -0800

    set start and end date to nullable for portfolio model

--- portfolio/portfolio.go
+++ portfolio/portfolio.go
@@ -6,19 +6,19 @@ import (
 
 // Portfolio
 type Portfolio struct {
-	ID              *int      `json:"id"`
-	UUID            string    `json:"uuid"`
-	Name            string    `json:"name"`
-	AlternateName   string    `json:"alternateName"`
-	StartDate       time.Time `json:"startDate"`
-	EndDate         time.Time `json:"endDate"`
-	UserEmail       string    `json:"userEmail"`
-	Description     string    `json:"description"`
-	BaseAssetID     *int      `json:"baseAssetId"`
-	PortfolioTypeID *int      `json:"portfolioTypeId"`
-	ParentID        *int      `json:"parentId"`
-	CreatedBy       string    `json:"createdBy"`
-	CreatedAt       time.Time `json:"createdAt"`
-	UpdatedBy       string    `json:"updatedBy"`
-	UpdatedAt       time.Time `json:"updatedAt"`
+	ID              *int       `json:"id"`
+	UUID            string     `json:"uuid"`
+	Name            string     `json:"name"`
+	AlternateName   string     `json:"alternateName"`
+	StartDate       *time.Time `json:"startDate"`
+	EndDate         *time.Time `json:"endDate"`
+	UserEmail       string     `json:"userEmail"`
+	Description     string     `json:"description"`
+	BaseAssetID     *int       `json:"baseAssetId"`
+	PortfolioTypeID *int       `json:"portfolioTypeId"`
+	ParentID        *int       `json:"parentId"`
+	CreatedBy       string     `json:"createdBy"`
+	CreatedAt       time.Time  `json:"createdAt"`
+	UpdatedBy       string     `json:"updatedBy"`
+	UpdatedAt       time.Time  `json:"updatedAt"`
 }
