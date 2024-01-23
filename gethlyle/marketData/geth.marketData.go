3101ebdfe2a4e6f74f685a8ccab95d0a1c336ed9
Keita Fukue <keitafukue@gmail.com>
2024-01-23 10:56:33 -0800

    change gethmarketdata fields to Decimal from float64

--- gethlyle/marketData/geth.marketData.go
+++ gethlyle/marketData/geth.marketData.go
@@ -2,35 +2,37 @@ package gethlylemarketdata
 
 import (
 	"time"
+
+	decimal "github.com/shopspring/decimal"
 )
 
 // MarketData
 type MarketData struct {
-	ID                *int      `json:"id"`
-	UUID              string    `json:"uuid"`
-	Name              string    `json:"name"`
-	AlternateName     string    `json:"alternateName"`
-	StartDate         time.Time `json:"startDate"`
-	EndDate           time.Time `json:"endDate"`
-	AssetID           *int      `json:"assetId"`
-	OpenUSD           *float64  `json:"openUsd"`
-	CloseUSD          *float64  `json:"closeUsd"`
-	HighUSD           *float64  `json:"highUsd"`
-	LowUSD            *float64  `json:"lowUsd"`
-	PriceUSD          *float64  `json:"priceUsd"`
-	VolumeUSD         *float64  `json:"volumeUsd"`
-	MarketCapUSD      *float64  `json:"marketCapUsd"`
-	Ticker            string    `json:"ticker"`
-	Description       string    `json:"description"`
-	IntervalID        *int      `json:"intervalId"`
-	MarketDataTypeID  *int      `json:"marketDataTypeId"`
-	SourceID          *int      `json:"sourceId"`
-	TotalSupply       *float64  `json:"totalSupply"`
-	MaxSupply         *float64  `json:"maxSupply"`
-	CirculatingSupply *float64  `json:"circulatingSupply"`
-	Sparkline7d       []float64 `json:"sparkline7d"`
-	CreatedBy         string    `json:"createdBy"`
-	CreatedAt         time.Time `json:"createdAt"`
-	UpdatedBy         string    `json:"updatedBy"`
-	UpdatedAt         time.Time `json:"updatedAt"`
+	ID                *int              `json:"id"`
+	UUID              string            `json:"uuid"`
+	Name              string            `json:"name"`
+	AlternateName     string            `json:"alternateName"`
+	StartDate         time.Time         `json:"startDate"`
+	EndDate           time.Time         `json:"endDate"`
+	AssetID           *int              `json:"assetId"`
+	OpenUSD           *decimal.Decimal  `json:"openUsd"`
+	CloseUSD          *decimal.Decimal  `json:"closeUsd"`
+	HighUSD           *decimal.Decimal  `json:"highUsd"`
+	LowUSD            *decimal.Decimal  `json:"lowUsd"`
+	PriceUSD          *decimal.Decimal  `json:"priceUsd"`
+	VolumeUSD         *decimal.Decimal  `json:"volumeUsd"`
+	MarketCapUSD      *decimal.Decimal  `json:"marketCapUsd"`
+	Ticker            string            `json:"ticker"`
+	Description       string            `json:"description"`
+	IntervalID        *int              `json:"intervalId"`
+	MarketDataTypeID  *int              `json:"marketDataTypeId"`
+	SourceID          *int              `json:"sourceId"`
+	TotalSupply       *decimal.Decimal  `json:"totalSupply"`
+	MaxSupply         *decimal.Decimal  `json:"maxSupply"`
+	CirculatingSupply *decimal.Decimal  `json:"circulatingSupply"`
+	Sparkline7d       []decimal.Decimal `json:"sparkline7d"`
+	CreatedBy         string            `json:"createdBy"`
+	CreatedAt         time.Time         `json:"createdAt"`
+	UpdatedBy         string            `json:"updatedBy"`
+	UpdatedAt         time.Time         `json:"updatedAt"`
 }
