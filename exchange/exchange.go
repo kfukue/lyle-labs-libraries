13048e9d0ac82b348b6801bfe48e0f9bbfba2e66
Keita Fukue <keitafukue@gmail.com>
2024-06-26 18:09:43 -0700

    Update exchange for unit testing/pgx

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -5,28 +5,28 @@ import (
 )
 
 type Exchange struct {
-	ID             *int       `json:"id"`
-	UUID           string     `json:"uuid"`
-	Name           string     `json:"name"`
-	AlternateName  string     `json:"alternateName"`
-	ExchangeTypeID *int       `json:"exchangeTypeId"`
-	Url            string     `json:"url"`
-	StartDate      *time.Time `json:"startDate"`
-	EndDate        *time.Time `json:"endDate"`
-	Description    string     `json:"description"`
-	CreatedBy      string     `json:"createdBy"`
-	CreatedAt      time.Time  `json:"createdAt"`
-	UpdatedBy      string     `json:"updatedBy"`
-	UpdatedAt      time.Time  `json:"updatedAt"`
+	ID             *int       `json:"id" db:"id"`
+	UUID           string     `json:"uuid" db:"uuid"`
+	Name           string     `json:"name" db:"name" db:"name"`
+	AlternateName  string     `json:"alternateName" db:"alternate_name"`
+	ExchangeTypeID *int       `json:"exchangeTypeId" db:"exchange_type_id"`
+	Url            string     `json:"url" db:"url"`
+	StartDate      *time.Time `json:"startDate" db:"start_date"`
+	EndDate        *time.Time `json:"endDate" db:"end_date"`
+	Description    string     `json:"description" db:"description"`
+	CreatedBy      string     `json:"createdBy" db:"created_by"`
+	CreatedAt      time.Time  `json:"createdAt" db:"created_at"`
+	UpdatedBy      string     `json:"updatedBy" db:"updated_by"`
+	UpdatedAt      time.Time  `json:"updatedAt" db:"updated_at"`
 }
 
 type ExchangeChain struct {
-	UUID        string    `json:"uuid"`
-	ExchangeID  *int      `json:"exchangeId"`
-	ChainID     *int      `json:"chainId"`
-	Description string    `json:"description"`
-	CreatedBy   string    `json:"createdBy"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedBy   string    `json:"updatedBy"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	UUID        string    `json:"uuid" db:"uuid"`
+	ExchangeID  *int      `json:"exchangeId" db:"exchange_id"`
+	ChainID     *int      `json:"chainId" db:"chain_id"`
+	Description string    `json:"description" db:"description"`
+	CreatedBy   string    `json:"createdBy" db:"created_by"`
+	CreatedAt   time.Time `json:"createdAt" db:"created_at"`
+	UpdatedBy   string    `json:"updatedBy" db:"updated_by"`
+	UpdatedAt   time.Time `json:"updatedAt" db:"updated_at"`
 }
