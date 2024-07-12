672852aaf5dba35ed2b245a20354fc402c43140b
Keita Fukue <keitafukue@gmail.com>
2024-07-11 17:25:59 -0700

    Update job related models for unit testing/pgx

--- job/job.go
+++ job/job.go
@@ -9,25 +9,25 @@ import (
 
 // Job
 type Job struct {
-	ID               *int      `json:"id"`
-	UUID             string    `json:"uuid"`
-	Name             string    `json:"name"`
-	AlternateName    string    `json:"alternateName"`
-	StartDate        time.Time `json:"startDate"`
-	EndDate          time.Time `json:"endDate"`
-	Description      string    `json:"description"`
-	StatusID         *int      `json:"statusId"`
-	ResponseStatus   string    `json:"responseStatus"`
-	RequestUrl       string    `json:"requestUrl"`
-	RequestBody      string    `json:"requestBody"`
-	RequestMethod    string    `json:"requestMethod"`
-	ResponseData     string    `json:"responseData"`
-	ResponseDataJson Attrs     `json:"responseDataJson"`
-	JobCategoryID    *int      `json:"jobCategoryId"`
-	CreatedBy        string    `json:"createdBy"`
-	CreatedAt        time.Time `json:"createdAt"`
-	UpdatedBy        string    `json:"updatedBy"`
-	UpdatedAt        time.Time `json:"updatedAt"`
+	ID               *int      `json:"id" db:"id"`                               //1
+	UUID             string    `json:"uuid" db:"uuid"`                           //2
+	Name             string    `json:"name" db:"name"`                           //3
+	AlternateName    string    `json:"alternateName" db:"alternate_name"`        //4
+	StartDate        time.Time `json:"startDate" db:"start_date"`                //5
+	EndDate          time.Time `json:"endDate" db:"end_date"`                    //6
+	Description      string    `json:"description" db:"description"`             //7
+	StatusID         *int      `json:"statusId" db:"status_id"`                  //8
+	ResponseStatus   string    `json:"responseStatus" db:"response_status"`      //9
+	RequestUrl       string    `json:"requestUrl" db:"request_url"`              //10
+	RequestBody      string    `json:"requestBody" db:"request_body"`            //11
+	RequestMethod    string    `json:"requestMethod" db:"request_method"`        //12
+	ResponseData     string    `json:"responseData" db:"response_data"`          //13
+	ResponseDataJson Attrs     `json:"responseDataJson" db:"response_data_json"` //14
+	JobCategoryID    *int      `json:"jobCategoryId" db:"job_category_id"`       //15
+	CreatedBy        string    `json:"createdBy" db:"created_by"`                //16
+	CreatedAt        time.Time `json:"createdAt" db:"created_at"`                //17
+	UpdatedBy        string    `json:"updatedBy" db:"updated_by"`                //18
+	UpdatedAt        time.Time `json:"updatedAt" db:"updated_at"`                //19
 }
 
 type Attrs map[string]interface{}
