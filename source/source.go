b7582e4ea8df6d4b54a2b0fd07a6ed39b245155d
Keita Fukue <keitafukue@gmail.com>
2024-08-06 18:14:31 -0700

    Update source related models for unit testing/pgx

--- source/source.go
+++ source/source.go
@@ -6,15 +6,15 @@ import (
 
 // Source
 type Source struct {
-	ID            *int      `json:"id"`
-	UUID          string    `json:"uuid"`
-	Name          string    `json:"name"`
-	AlternateName string    `json:"alternateName"`
-	URL           string    `json:"url"`
-	Ticker        string    `json:"ticker"`
-	Description   string    `json:"description"`
-	CreatedBy     string    `json:"createdBy"`
-	CreatedAt     time.Time `json:"createdAt"`
-	UpdatedBy     string    `json:"updatedBy"`
-	UpdatedAt     time.Time `json:"updatedAt"`
+	ID            *int      `json:"id" db:"id"`                        //1
+	UUID          string    `json:"uuid" db:"uuid"`                    //2
+	Name          string    `json:"name" db:"name"`                    //3
+	AlternateName string    `json:"alternateName" db:"alternate_name"` //4
+	URL           string    `json:"url" db:"url"`                      //5
+	Ticker        string    `json:"ticker" db:"ticker"`                //6
+	Description   string    `json:"description" db:"description"`      //7
+	CreatedBy     string    `json:"createdBy" db:"created_by"`         //8
+	CreatedAt     time.Time `json:"createdAt" db:"created_at"`         //9
+	UpdatedBy     string    `json:"updatedBy" db:"updated_by"`         //10
+	UpdatedAt     time.Time `json:"updatedAt" db:"updated_at"`         //11
 }
