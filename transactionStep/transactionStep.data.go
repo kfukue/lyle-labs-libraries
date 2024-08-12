b32f6660725977c7b77b93ddb5984bcf3025aea0
Keita Fukue <keitafukue@gmail.com>
2024-08-12 14:55:33 -0700

    Update transactionStep related models for unit testing/pgx

--- transactionStep/transactionStep.data.go
+++ transactionStep/transactionStep.data.go
@@ -10,55 +10,46 @@ import (
 	"github.com/gofrs/uuid"
 	"github.com/jackc/pgtype"
 	"github.com/jackc/pgx/v5"
-	"github.com/kfukue/lyle-labs-libraries/database"
+	"github.com/kfukue/lyle-labs-libraries/utils"
 	"github.com/lib/pq"
 )
 
-func GetTransactionStep(transactionID int, stepID int) (*TransactionStep, error) {
+func GetTransactionStep(dbConnPgx utils.PgxIface, transactionID, stepID *int) (*TransactionStep, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
 	defer cancel()
-	row := database.DbConnPgx.QueryRow(ctx, `SELECT 
-	transaction_id,  
-	step_id,
-	uuid, 
-	name,
-	alternate_name,
-	description,
-	created_by, 
-	created_at, 
-	updated_by, 
-	updated_at 
+	row, err := dbConnPgx.Query(ctx, `SELECT
+		transaction_id,  
+		step_id,
+		uuid, 
+		name,
+		alternate_name,
+		description,
+		created_by, 
+		created_at, 
+		updated_by, 
+		updated_at 
 	FROM transaction_steps 
 	WHERE transaction_id = $1
 	AND step_id = $2
-	`, transactionID, stepID)
-
-	transactionStep := &TransactionStep{}
-	err := row.Scan(
-		&transactionStep.TransactionID,
-		&transactionStep.StepID,
-		&transactionStep.UUID,
-		&transactionStep.Name,
-		&transactionStep.AlternateName,
-		&transactionStep.Description,
-		&transactionStep.CreatedBy,
-		&transactionStep.CreatedAt,
-		&transactionStep.UpdatedBy,
-		&transactionStep.UpdatedAt,
-	)
+	`, *transactionID, *stepID)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+	transactionStep, err := pgx.CollectOneRow(row, pgx.RowToStructByName[TransactionStep])
 	if errors.Is(err, pgx.ErrNoRows) {
 		return nil, nil
 	} else if err != nil {
 		log.Println(err)
 		return nil, err
 	}
-	return transactionStep, nil
+	return &transactionStep, nil
 }
 
-func GetTransactionStepByUUID(transactionStepUUID string) (*TransactionStep, error) {
+func GetTransactionStepByUUID(dbConnPgx utils.PgxIface, transactionStepUUID string) (*TransactionStep, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
 	defer cancel()
-	row := database.DbConnPgx.QueryRow(ctx, `SELECT 
+	row, err := dbConnPgx.Query(ctx, `SELECT
 	transaction_id,  
 	step_id,
 	uuid, 
@@ -72,33 +63,24 @@ func GetTransactionStepByUUID(transactionStepUUID string) (*TransactionStep, err
 	FROM transaction_steps 
 	WHERE text(uuid) = $1
 	`, transactionStepUUID)
-
-	transactionStep := &TransactionStep{}
-	err := row.Scan(
-		&transactionStep.TransactionID,
-		&transactionStep.StepID,
-		&transactionStep.UUID,
-		&transactionStep.Name,
-		&transactionStep.AlternateName,
-		&transactionStep.Description,
-		&transactionStep.CreatedBy,
-		&transactionStep.CreatedAt,
-		&transactionStep.UpdatedBy,
-		&transactionStep.UpdatedAt,
-	)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+	transactionStep, err := pgx.CollectOneRow(row, pgx.RowToStructByName[TransactionStep])
 	if errors.Is(err, pgx.ErrNoRows) {
 		return nil, nil
 	} else if err != nil {
 		log.Println(err)
 		return nil, err
 	}
-	return transactionStep, nil
+	return &transactionStep, nil
 }
 
-func GetTransactionStepsByUUIDs(UUIDList []string) ([]TransactionStep, error) {
+func GetTransactionStepsByUUIDs(dbConnPgx utils.PgxIface, UUIDList []string) ([]TransactionStep, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
 	defer cancel()
-	results, err := database.DbConnPgx.Query(ctx, `SELECT 
+	results, err := dbConnPgx.Query(ctx, `SELECT 
 	transaction_id,  
 	step_id,
 	uuid, 
@@ -117,98 +99,53 @@ func GetTransactionStepsByUUIDs(UUIDList []string) ([]TransactionStep, error) {
 		return nil, err
 	}
 	defer results.Close()
-	transactionStepList := make([]TransactionStep, 0)
-	for results.Next() {
-		var transactionStep TransactionStep
-		results.Scan(
-			&transactionStep.TransactionID,
-			&transactionStep.StepID,
-			&transactionStep.UUID,
-			&transactionStep.Name,
-			&transactionStep.AlternateName,
-			&transactionStep.Description,
-			&transactionStep.CreatedBy,
-			&transactionStep.CreatedAt,
-			&transactionStep.UpdatedBy,
-			&transactionStep.UpdatedAt,
-		)
-
-		transactionStepList = append(transactionStepList, transactionStep)
+	transactionStepList, err := pgx.CollectRows(results, pgx.RowToStructByName[TransactionStep])
+	if err != nil {
+		log.Println(err)
+		return nil, err
 	}
 	return transactionStepList, nil
 }
 
-func GetTopTenTransactionSteps() ([]TransactionStep, error) {
+func RemoveTransactionStep(dbConnPgx utils.PgxIface, transactionID, stepID *int) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
 	defer cancel()
-	results, err := database.DbConnPgx.Query(ctx, `SELECT 
-	transaction_id,  
-	step_id,
-	uuid, 
-	name,
-	alternate_name,
-	description,
-	created_by, 
-	created_at, 
-	updated_by, 
-	updated_at 
-	FROM transaction_steps 
-	`)
+	tx, err := dbConnPgx.Begin(ctx)
 	if err != nil {
-		log.Println(err.Error())
-		return nil, err
-	}
-	defer results.Close()
-	transactionSteps := make([]TransactionStep, 0)
-	for results.Next() {
-		var transactionStep TransactionStep
-		results.Scan(
-			&transactionStep.TransactionID,
-			&transactionStep.StepID,
-			&transactionStep.UUID,
-			&transactionStep.Name,
-			&transactionStep.AlternateName,
-			&transactionStep.Description,
-			&transactionStep.CreatedBy,
-			&transactionStep.CreatedAt,
-			&transactionStep.UpdatedBy,
-			&transactionStep.UpdatedAt,
-		)
-
-		transactionSteps = append(transactionSteps, transactionStep)
-	}
-	return transactionSteps, nil
-}
-
-func RemoveTransactionStep(transactionID int, stepID int) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
-	defer cancel()
-	_, err := database.DbConnPgx.Query(ctx, `DELETE FROM transaction_steps WHERE 
-	transaction_id = $1 AND step_id =$2`, transactionID, stepID)
-	if err != nil {
-		log.Println(err.Error())
+		log.Printf("Error in RemoveTransactionStep DbConn.Begin   %s", err.Error())
 		return err
 	}
-	return nil
-}
-
-func RemoveTransactionStepByUUID(transactionStepUUID string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
-	defer cancel()
-	_, err := database.DbConnPgx.Query(ctx, `DELETE FROM transaction_steps WHERE 
-		WHERE text(uuid) = $1`,
-		transactionStepUUID)
-	if err != nil {
-		log.Println(err.Error())
+	sql := `DELETE FROM transaction_steps WHERE 
+		transaction_id = $1 AND step_id =$2`
+	defer dbConnPgx.Close()
+	if _, err := dbConnPgx.Exec(ctx, sql, *transactionID, *stepID); err != nil {
+		tx.Rollback(ctx)
 		return err
 	}
-	return nil
+	return tx.Commit(ctx)
 }
 
-func GetTransactionStepList() ([]TransactionStep, error) {
+func RemoveTransactionStepByUUID(dbConnPgx utils.PgxIface, transactionStepUUID string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
 	defer cancel()
-	results, err := database.DbConnPgx.Query(ctx, `SELECT 
+	tx, err := dbConnPgx.Begin(ctx)
+	if err != nil {
+		log.Printf("Error in RemoveTransactionStepByUUID DbConn.Begin   %s", err.Error())
+		return err
+	}
+	sql := `DELETE FROM transaction_steps WHERE text(uuid) = $1`
+	defer dbConnPgx.Close()
+	if _, err := dbConnPgx.Exec(ctx, sql, transactionStepUUID); err != nil {
+		tx.Rollback(ctx)
+		return err
+	}
+	return tx.Commit(ctx)
+}
+
+func GetTransactionStepList(dbConnPgx utils.PgxIface) ([]TransactionStep, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
+	defer cancel()
+	results, err := dbConnPgx.Query(ctx, `SELECT 
 	transaction_id,  
 	step_id,
 	uuid, 
@@ -225,89 +162,92 @@ func GetTransactionStepList() ([]TransactionStep, error) {
 		return nil, err
 	}
 	defer results.Close()
-	transactionSteps := make([]TransactionStep, 0)
-	for results.Next() {
-		var transactionStep TransactionStep
-		results.Scan(
-			&transactionStep.TransactionID,
-			&transactionStep.StepID,
-			&transactionStep.UUID,
-			&transactionStep.Name,
-			&transactionStep.AlternateName,
-			&transactionStep.Description,
-			&transactionStep.CreatedBy,
-			&transactionStep.CreatedAt,
-			&transactionStep.UpdatedBy,
-			&transactionStep.UpdatedAt,
-		)
-
-		transactionSteps = append(transactionSteps, transactionStep)
+	transactionSteps, err := pgx.CollectRows(results, pgx.RowToStructByName[TransactionStep])
+	if err != nil {
+		log.Println(err)
+		return nil, err
 	}
 	return transactionSteps, nil
 }
 
-func UpdateTransactionStep(transactionStep TransactionStep) error {
+func UpdateTransactionStep(dbConnPgx utils.PgxIface, transactionStep *TransactionStep) error {
 	// if the transactionStep id is set, update, otherwise add
 	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
 	defer cancel()
 	if (transactionStep.TransactionID == nil || *transactionStep.TransactionID == 0) || (transactionStep.StepID == nil || *transactionStep.StepID == 0) {
 		return errors.New("transactionStep has invalid ID")
 	}
-	_, err := database.DbConnPgx.Query(ctx, `UPDATE transaction_steps SET 
+	tx, err := dbConnPgx.Begin(ctx)
+	if err != nil {
+		log.Printf("Error in UpdateTransactionStep DbConn.Begin   %s", err.Error())
+		return err
+	}
+	sql := `UPDATE transaction_steps SET 
 		name=$1,
 		alternate_name=$2,
 		description=$3,
 		updated_by=$4, 
 		updated_at=current_timestamp at time zone 'UTC'
-		WHERE transaction_id=$5 AND step_id=$6`,
-
+		WHERE transaction_id=$5 AND step_id=$6`
+	defer dbConnPgx.Close()
+	if _, err := dbConnPgx.Exec(ctx, sql,
 		transactionStep.Name,          //1
 		transactionStep.AlternateName, //2
 		transactionStep.Description,   //3
 		transactionStep.UpdatedBy,     //4
 		transactionStep.TransactionID, //5
 		transactionStep.StepID,        //6
-	)
-	if err != nil {
-		log.Println(err.Error())
+	); err != nil {
+		tx.Rollback(ctx)
 		return err
 	}
-	return nil
+	return tx.Commit(ctx)
 }
 
-func UpdateTransactionStepByUUID(transactionStep TransactionStep) error {
+func UpdateTransactionStepByUUID(dbConnPgx utils.PgxIface, transactionStep *TransactionStep) error {
 	// if the transactionStep id is set, update, otherwise add
 	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
 	defer cancel()
 	if (transactionStep.TransactionID == nil || *transactionStep.TransactionID == 0) || (transactionStep.StepID == nil || *transactionStep.StepID == 0) {
 		return errors.New("transactionStep has invalid ID")
 	}
-	_, err := database.DbConnPgx.Query(ctx, `UPDATE transaction_steps SET 
+	tx, err := dbConnPgx.Begin(ctx)
+	if err != nil {
+		log.Printf("Error in UpdateTransactionStepByUUID DbConn.Begin   %s", err.Error())
+		return err
+	}
+	sql := `UPDATE transaction_steps SET 
 		name=$1,
 		alternate_name=$2,
 		description=$3,
 		updated_by=$4,
 		updated_at=current_timestamp at time zone 'UTC'
 		WHERE text(uuid) = $5
-		`,
+		`
+	defer dbConnPgx.Close()
+	if _, err := dbConnPgx.Exec(ctx, sql,
 		transactionStep.Name,          //1
 		transactionStep.AlternateName, //2
 		transactionStep.Description,   //3
 		transactionStep.UpdatedBy,     //4
 		transactionStep.UUID,          //5
-	)
-	if err != nil {
-		log.Println(err.Error())
+	); err != nil {
+		tx.Rollback(ctx)
 		return err
 	}
-	return nil
+	return tx.Commit(ctx)
 }
 
-func InsertTransactionStep(transactionStep TransactionStep) (int, int, error) {
+func InsertTransactionStep(dbConnPgx utils.PgxIface, transactionStep *TransactionStep) (int, int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
 	defer cancel()
+	tx, err := dbConnPgx.Begin(ctx)
+	if err != nil {
+		log.Printf("Error in InsertTransactionStep DbConn.Begin   %s", err.Error())
+		return -1, -1, err
+	}
 	var MarketDataID int
-	var JobID int
+	var StepID int
 	transactionStepUUID, err := uuid.NewV4()
 	if err != nil {
 		log.Fatalf("failed to generate UUID: %v", err)
@@ -315,7 +255,7 @@ func InsertTransactionStep(transactionStep TransactionStep) (int, int, error) {
 	if transactionStep.UUID == "" {
 		transactionStep.UUID = transactionStepUUID.String()
 	}
-	err = database.DbConnPgx.QueryRow(ctx, `INSERT INTO transaction_steps  
+	err = dbConnPgx.QueryRow(ctx, `INSERT INTO transaction_steps  
 	(
 		transaction_id,  
 		step_id,
@@ -347,15 +287,22 @@ func InsertTransactionStep(transactionStep TransactionStep) (int, int, error) {
 		transactionStep.AlternateName, //5
 		transactionStep.Description,   //6
 		transactionStep.CreatedBy,     //7
-	).Scan(&MarketDataID, &JobID)
+	).Scan(&MarketDataID, &StepID)
 	if err != nil {
+		tx.Rollback(ctx)
 		log.Println(err.Error())
-		return 0, 0, err
+		return -1, -1, err
 	}
-	return int(MarketDataID), int(JobID), nil
+	err = tx.Commit(ctx)
+	if err != nil {
+		tx.Rollback(ctx)
+		log.Println(err.Error())
+		return -1, -1, err
+	}
+	return int(MarketDataID), int(StepID), nil
 }
 
-func InsertTransactionSteps(transactionSteps []TransactionStep) error {
+func InsertTransactionSteps(dbConnPgx utils.PgxIface, transactionSteps []TransactionStep) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
 	defer cancel()
 	loc, _ := time.LoadLocation("UTC")
@@ -363,27 +310,25 @@ func InsertTransactionSteps(transactionSteps []TransactionStep) error {
 	rows := [][]interface{}{}
 	for i := range transactionSteps {
 		transactionStep := transactionSteps[i]
-
 		uuidString := &pgtype.UUID{}
 		uuidString.Set(transactionStep.UUID)
 		row := []interface{}{
-
-			*transactionStep.TransactionID, //1
-			*transactionStep.StepID,        //2
-			uuidString,                     //3
-			transactionStep.Name,           //4
-			transactionStep.AlternateName,  //5
-			transactionStep.Description,    //6
-			transactionStep.CreatedBy,      //7
-			&transactionStep.CreatedAt,     //8
-			transactionStep.CreatedBy,      //9
-			&now,                           //10
+			transactionStep.TransactionID, //1
+			transactionStep.StepID,        //2
+			uuidString,                    //3
+			transactionStep.Name,          //4
+			transactionStep.AlternateName, //5
+			transactionStep.Description,   //6
+			transactionStep.CreatedBy,     //7
+			&transactionStep.CreatedAt,    //8
+			transactionStep.CreatedBy,     //9
+			&now,                          //10
 		}
 		rows = append(rows, row)
 	}
 	// Given db is a *sql.DB
 
-	copyCount, err := database.DbConnPgx.CopyFrom(
+	copyCount, err := dbConnPgx.CopyFrom(
 		ctx,
 		pgx.Identifier{"transaction_steps"},
 		[]string{
@@ -400,11 +345,79 @@ func InsertTransactionSteps(transactionSteps []TransactionStep) error {
 		},
 		pgx.CopyFromRows(rows),
 	)
-	log.Println(fmt.Printf("copy count: %d", copyCount))
+	log.Println(fmt.Printf("InsertTransactionSteps: copy count: %d", copyCount))
 	if err != nil {
-		log.Fatal(err)
-		// handle error that occurred while using *pgx.Conn
+		log.Println(err.Error())
+		return err
 	}
 
 	return nil
+}
+
+// for refinedev
+func GetTransactionStepListByPagination(dbConnPgx utils.PgxIface, _start, _end *int, _order, _sort string, _filters []string) ([]TransactionStep, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
+	defer cancel()
+	sql := `
+	SELECT 
+	transaction_id,  
+	step_id,
+	uuid, 
+	name,
+	alternate_name,
+	description,
+	created_by, 
+	created_at, 
+	updated_by, 
+	updated_at 
+	FROM transaction_steps 
+	`
+	if len(_filters) > 0 {
+		sql += "WHERE "
+		for i, filter := range _filters {
+			sql += filter
+			if i < len(_filters)-1 {
+				sql += " OR "
+			}
+		}
+	}
+	if _order != "" && _sort != "" {
+		sql += fmt.Sprintf(" ORDER BY %s %s ", _sort, _order)
+	}
+	if (_start != nil && *_start > 0) && (_end != nil && *_end > 0) {
+		pageSize := *_end - *_start
+		sql += fmt.Sprintf(" OFFSET %d LIMIT %d ", *_start, pageSize)
+	}
+
+	results, err := dbConnPgx.Query(ctx, sql)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+	defer results.Close()
+	transactionSteps, err := pgx.CollectRows(results, pgx.RowToStructByName[TransactionStep])
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return transactionSteps, nil
+}
+
+func GetTotalTransactionStepsCount(dbConnPgx utils.PgxIface) (*int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
+	defer cancel()
+
+	row := dbConnPgx.QueryRow(ctx, `SELECT 
+	COUNT(*)
+	FROM transaction_steps
+	`)
+	totalCount := 0
+	err := row.Scan(
+		&totalCount,
+	)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return &totalCount, nil
 }
--- transactionStep/transactionStep.go
+++ transactionStep/transactionStep.go
@@ -9,16 +9,16 @@ import (
 
 // Asset
 type TransactionStep struct {
-	TransactionID *int      `json:"transactionId"` //1
-	StepID        *int      `json:"stepId"`        //2
-	UUID          string    `json:"uuid"`          //3
-	Name          string    `json:"name"`          //4
-	AlternateName string    `json:"alternateName"` //5
-	Description   string    `json:"description"`   //6
-	CreatedBy     string    `json:"createdBy"`     //7
-	CreatedAt     time.Time `json:"createdAt"`     //8
-	UpdatedBy     string    `json:"updatedBy"`     //9
-	UpdatedAt     time.Time `json:"updatedAt"`     //10
+	TransactionID *int      `json:"transactionId" db:"transaction_id"` //1
+	StepID        *int      `json:"stepId" db:"step_id"`               //2
+	UUID          string    `json:"uuid" db:"uuid"`                    //3
+	Name          string    `json:"name" db:"name"`                    //4
+	AlternateName string    `json:"alternateName" db:"alternate_name"` //5
+	Description   string    `json:"description" db:"description"`      //6
+	CreatedBy     string    `json:"createdBy" db:"created_by"`         //7
+	CreatedAt     time.Time `json:"createdAt" db:"created_at"`         //8
+	UpdatedBy     string    `json:"updatedBy" db:"updated_by"`         //9
+	UpdatedAt     time.Time `json:"updatedAt" db:"updated_at"`         //10
 }
 
 type Attrs map[string]interface{}
