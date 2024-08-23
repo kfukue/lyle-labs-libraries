409dcf8c44c83fa1b66d68f6f5b4009657870ccf
Keita Fukue <keitafukue@gmail.com>
2024-08-22 17:28:20 -0700

    set chainlinkusdaddress as string pointer to make it nullable

--- asset/asset.go
+++ asset/asset.go
@@ -7,32 +7,32 @@ import (
 
 // Asset
 type Asset struct {
-	ID                  *int      `json:"id" db:"id"`
-	UUID                string    `json:"uuid" db:"uuid"`
-	Name                string    `json:"name" db:"name"`
-	AlternateName       string    `json:"alternateName" db:"alternate_name"`
-	Cusip               string    `json:"cusip" db:"cusip"`
-	Ticker              string    `json:"ticker" db:"ticker"`
-	BaseAssetID         *int      `json:"baseAssetId" db:"base_asset_id"`
-	QuoteAssetID        *int      `json:"quoteAssetId" db:"quote_asset_id"`
-	Description         string    `json:"description" db:"description"`
-	AssetTypeID         *int      `json:"assetTypeId" db:"asset_type_id"`
-	CreatedBy           string    `json:"createdBy" db:"created_by"`
-	CreatedAt           time.Time `json:"createdAt" db:"created_at"`
-	UpdatedBy           string    `json:"updatedBy" db:"updated_by"`
-	UpdatedAt           time.Time `json:"updatedAt" db:"updated_at"`
-	ChainID             *int      `json:"chainId" db:"chain_id"`
-	CategoryID          *int      `json:"categoryId" db:"category_id"`
-	SubCategoryID       *int      `json:"subCategoryId" db:"sub_category_id"`
-	IsDefaultQuote      *bool     `json:"isDefaultQuote" db:"is_default_quote"`
-	IgnoreMarketData    *bool     `json:"ignoreMarketData" db:"ignore_market_data"`
-	Decimals            *int      `json:"decimals" db:"decimals"`
-	ContractAddress     string    `json:"contractAddress" db:"contract_address"`
-	StartingBlockNumber *uint64   `json:"startingBlockNumber" db:"starting_block_number"`
-	ImportGeth          *bool     `json:"importGeth" db:"import_geth"`
-	ImportGethInitial   *bool     `json:"importGethInitial" db:"import_geth_initial"`
-	ChainlinkUSDAddress string    `json:"chainlinkUSDAddress" db:"chainlink_usd_address"`
-	ChainlinkUSDChainID *int      `json:"chainlinkUSDChainId" db:"chainlink_usd_chain_id"`
+	ID                  *int      `json:"id" db:"id"`                                      //1
+	UUID                string    `json:"uuid" db:"uuid"`                                  //2
+	Name                string    `json:"name" db:"name"`                                  //3
+	AlternateName       string    `json:"alternateName" db:"alternate_name"`               //4
+	Cusip               string    `json:"cusip" db:"cusip"`                                //5
+	Ticker              string    `json:"ticker" db:"ticker"`                              //6
+	BaseAssetID         *int      `json:"baseAssetId" db:"base_asset_id"`                  //7
+	QuoteAssetID        *int      `json:"quoteAssetId" db:"quote_asset_id"`                //8
+	Description         string    `json:"description" db:"description"`                    //9
+	AssetTypeID         *int      `json:"assetTypeId" db:"asset_type_id"`                  //10
+	CreatedBy           string    `json:"createdBy" db:"created_by"`                       //11
+	CreatedAt           time.Time `json:"createdAt" db:"created_at"`                       //12
+	UpdatedBy           string    `json:"updatedBy" db:"updated_by"`                       //13
+	UpdatedAt           time.Time `json:"updatedAt" db:"updated_at"`                       //14
+	ChainID             *int      `json:"chainId" db:"chain_id"`                           //15
+	CategoryID          *int      `json:"categoryId" db:"category_id"`                     //16
+	SubCategoryID       *int      `json:"subCategoryId" db:"sub_category_id"`              //17
+	IsDefaultQuote      *bool     `json:"isDefaultQuote" db:"is_default_quote"`            //18
+	IgnoreMarketData    *bool     `json:"ignoreMarketData" db:"ignore_market_data"`        //19
+	Decimals            *int      `json:"decimals" db:"decimals"`                          //20
+	ContractAddress     string    `json:"contractAddress" db:"contract_address"`           //21
+	StartingBlockNumber *uint64   `json:"startingBlockNumber" db:"starting_block_number"`  //22
+	ImportGeth          *bool     `json:"importGeth" db:"import_geth"`                     //23
+	ImportGethInitial   *bool     `json:"importGethInitial" db:"import_geth_initial"`      //24
+	ChainlinkUSDAddress *string   `json:"chainlinkUSDAddress" db:"chainlink_usd_address"`  //25
+	ChainlinkUSDChainID *int      `json:"chainlinkUSDChainId" db:"chainlink_usd_chain_id"` //26
 }
 
 // Asset
